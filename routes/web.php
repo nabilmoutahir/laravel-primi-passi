2f33ed9c7281c48adde6a0f872222f9284c0da35
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-20 20:23:58 +0100

    add var and compact fun

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $title = 'Hello World!';
+
+    return view('home', compact('title'));
 });
